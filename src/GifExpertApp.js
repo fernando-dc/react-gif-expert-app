9ac8bc1e4feacb7a3aa99a06a6d1c49fbbaec7d6
FernandoDC <fernando777dc@gmail.com>
2022-01-28 16:51:36 -0300

    Changed default initial query

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -5,7 +5,7 @@ import { GifGrid } from './components/GifGrid';
 export const GifExpertApp = () => {
 
 	// const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
-	const [categories, setCategories] = useState(['Gintama']);
+	const [categories, setCategories] = useState(['Dragon Ball']);
 
 	return (
 		<>
